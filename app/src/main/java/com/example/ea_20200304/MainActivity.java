bec14a35b47c4058eeca0a8a2a4d88092f023837
sstranahan <Anjanath15%>
2021-03-04 16:54:15 -0500

    update

--- app/src/main/java/com/example/ea_20200304/MainActivity.java
+++ app/src/main/java/com/example/ea_20200304/MainActivity.java
@@ -1,29 +1,62 @@
 package com.example.ea_20200304;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
+    private long FASTEST_INTERVAL = 2000; /* 2 sec */
 
     ImageButton homeButton;
     ImageButton contactsButton;
     ImageButton settingsButton;
     ImageButton mapButton;
 
+    static int googleAvail;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationRequest mLocationRequest;
+    public static LatLng latLng;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        homeButton = (ImageButton)findViewById(R.id.homeButton);
-        contactsButton = (ImageButton)findViewById(R.id.contactsButton);
-        settingsButton = (ImageButton)findViewById(R.id.settingsButton);
-        mapButton = (ImageButton)findViewById(R.id.mapsButton);
+        homeButton = (ImageButton) findViewById(R.id.homeButton);
+        contactsButton = (ImageButton) findViewById(R.id.contactsButton);
+        settingsButton = (ImageButton) findViewById(R.id.settingsButton);
+        mapButton = (ImageButton) findViewById(R.id.mapsButton);
 
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,5 +89,115 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        checkApiAvail();
+        checkReqPermissions();
+        startLocationUpdates();
+
+    }
+
+    private void checkApiAvail() {
+        // Check if google play services available
+        GoogleApiAvailability gApiAvail = new GoogleApiAvailability();
+        googleAvail = gApiAvail.isGooglePlayServicesAvailable(getApplicationContext());
+        if (googleAvail != ConnectionResult.SUCCESS) {
+            System.out.println("Not avail");
+            System.exit(99);
+        } else {
+            System.out.println("avail");
+        }
+    }
+
+    private void checkReqPermissions() {
+        // Check if permissions granted - if not, request them
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]{"location services permissions"}, 1);
+            return;
+        } else {
+            Toast
+                    .makeText(MainActivity.this,
+                            "Permissions granted",
+                            Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    // Trigger new location updates at interval
+// Trigger new location updates at interval
+    @SuppressLint("MissingPermission")
+    protected void startLocationUpdates() {
+
+        // Create the location request to start receiving updates
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        // Create LocationSettingsRequest object using location request
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+
+        // Check whether location settings are satisfied
+        // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+        settingsClient.checkLocationSettings(locationSettingsRequest);
+
+        // new Google API SDK v11 uses getFusedLocationProviderClient(this)
+        getFusedLocationProviderClient(getApplicationContext()).requestLocationUpdates(mLocationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        // do work here
+                        onLocationChanged(locationResult.getLastLocation());
+                    }
+                },
+                Looper.myLooper());
+    }
+
+    public void onLocationChanged(Location location) {
+        // New location has now been determined
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        // You can now create a LatLng Object for use with maps
+        latLng = new LatLng(location.getLatitude(), location.getLongitude());
+    }
+
+    @SuppressLint("MissingPermission")
+    public void getLastLocation() {
+        // Get last known recent location using new Google Play Services SDK (v11+)
+        FusedLocationProviderClient locationClient = getFusedLocationProviderClient(this);
+
+        locationClient.getLastLocation()
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // GPS location can be null if GPS is switched off
+                        if (location != null) {
+                            onLocationChanged(location);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("MapDemoActivity", "Error trying to get last GPS location");
+                        e.printStackTrace();
+                    }
+                });
+
+    }
+    public static LatLng getLatLng () {
+        return latLng;
     }
 }
+
+
+
--- app/src/main/java/com/example/ea_20200304/MapsActivity.java
+++ app/src/main/java/com/example/ea_20200304/MapsActivity.java
@@ -2,7 +2,10 @@ package com.example.ea_20200304;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,6 +17,11 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    ImageButton homeButton;
+    ImageButton contactsButton;
+    ImageButton settingsButton;
+    ImageButton mapButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
